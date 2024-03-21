ddf4059040440e9e4ae127bc5cca8c58d7f3ef76
25ejsb <eitan.brochstein@icloud.com>
2024-03-20 20:51:22 -0400

    lol

--- app.py
+++ app.py
@@ -47,14 +47,16 @@ class ContactForm(FlaskForm):
 #             msg=f"Subject:{subject}\n\n{message}"
 #         )
 
-# from urllib.parse import urlparse, urlunparse
-# @app.before_request
-# def redirect_nonwww():
-#     urlparts = urlparse(request.url)
-#     if urlparts.netloc == 'laurashulmanbrochstein.org':
-#         urlparts_list = list(urlparts)
-#         urlparts_list[1] = 'www.laurashulmanbrochstein.org'
-#         return redirect(urlunparse(urlparts_list), code=301)
+from urllib.parse import urlparse, urlunparse
+
+@app.before_request
+def redirect_nonwww():
+    """Redirect non-www requests to www."""
+    urlparts = urlparse(request.url)
+    if urlparts.netloc == 'laurashulmanbrochstein.org':
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = 'www.laurashulmanbrochstein.org'
+        return redirect(urlunparse(urlparts_list), code=301
 
 @app.route('/')
 def hello():
